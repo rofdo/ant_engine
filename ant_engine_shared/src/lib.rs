19f1c68c1cdbd624c4dd3869764244a7b720924e
rofdo <rofdo.github@ewe.net>
2024-05-12 20:37:54 +0200

    sending state back

--- ant_engine_shared/src/lib.rs
+++ ant_engine_shared/src/lib.rs
@@ -1,5 +1,5 @@
+use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread::{self, JoinHandle};
-use std::sync::mpsc::{self, Sender};
 
 #[derive(Debug, PartialEq)]
 pub enum Command {
@@ -7,21 +7,23 @@ pub enum Command {
     Stop,
 }
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct State {
-    value: i32,
+    test_value: i32,
 }
 
 impl State {
     pub fn new() -> State {
-        State { value: 0 }
+        State { test_value: 0 }
     }
 }
 
 pub struct Game {
+    running: bool,
     state: State,
     step: usize,
-    command_channel: mpsc::Receiver<Command>,
-    running: bool,
+    command_channel: Receiver<Command>,
+    state_channel: Sender<State>,
 }
 
 impl Game {
@@ -36,7 +38,7 @@ impl Game {
     fn step(&mut self, commands: Vec<Command>) {
         for command in commands {
             match command {
-                Command::Add(value) => self.state.value += value,
+                Command::Add(value) => self.state.test_value += value,
                 Command::Stop => {
                     println!("Game stopped at step {}", self.step);
                     self.running = false;
@@ -47,10 +49,15 @@ impl Game {
         self.step += 1;
     }
 
+    fn send_state(&self) {
+        self.state_channel.send(self.state.clone()).unwrap();
+    }
+
     fn run(&mut self) {
         while self.running {
             let commands = self.receive_commands();
             self.step(commands);
+            self.send_state();
         }
     }
 }
@@ -58,37 +65,49 @@ impl Game {
 pub struct GameHandler {
     thread: JoinHandle<()>,
     command_channel: Sender<Command>,
+    state_channel: Receiver<State>,
 }
 
 impl GameHandler {
-    pub fn send(&self, command: Command) {
-        self.command_channel.send(command).unwrap();
+    pub fn send(&self, command: Command) -> Result<(), Box<dyn std::error::Error>> {
+        self.command_channel.send(command).map_err(|_| "Failed to send command".into())
     }
 
-    pub fn stop(self) {
-        self.send(Command::Stop);
+    pub fn stop(self) -> Result<(), Box<dyn std::error::Error>> {
+        self.send(Command::Stop)?;
         self.thread.join().unwrap();
+        Ok(())
+    }
+
+    pub fn last_state(&self) -> Result<State, Box<dyn std::error::Error>> {
+        self.state_channel.recv().map_err(|_| "Failed to receive state".into())
     }
 }
 
 impl Game {
-    fn new(rx: mpsc::Receiver<Command>) -> Game {
+    fn new(command_rx: mpsc::Receiver<Command>, state_tx: mpsc::Sender<State>) -> Game {
         Game {
+            running: true,
             state: State::new(),
             step: 0,
-            command_channel: rx,
-            running: true,
+            command_channel: command_rx,
+            state_channel: state_tx,
         }
     }
 
     pub fn start() -> GameHandler {
-        let (tx, rx) = mpsc::channel();
-        let mut game = Game::new(rx);
+        let (command_tx, command_rx) = mpsc::channel();
+        let (state_tx, state_rx) = mpsc::channel();
+        let mut game = Game::new(command_rx, state_tx);
 
         let thread = thread::spawn(move || {
             game.run();
         });
-        GameHandler { thread , command_channel: tx }
+        GameHandler {
+            thread,
+            command_channel: command_tx,
+            state_channel: state_rx,
+        }
     }
 }
 
@@ -98,9 +117,11 @@ mod tests {
 
     #[test]
     fn receive_commands() {
-        let (tx, rx) = mpsc::channel();
-        let game = Game::new(rx);
-        tx.send(Command::Add(2)).unwrap();
+        let (command_tx, command_rx) = mpsc::channel();
+        let (state_tx, _) = mpsc::channel();
+
+        let game = Game::new(command_rx, state_tx);
+        command_tx.send(Command::Add(2)).unwrap();
 
         let commands = game.receive_commands();
         assert_eq!(commands.len(), 1);
@@ -110,8 +131,17 @@ mod tests {
     #[test]
     fn start_stop_game() {
         let game_handler = Game::start();
-        game_handler.send(Command::Add(2));
-        game_handler.send(Command::Add(3));
-        game_handler.stop();
+        game_handler.send(Command::Add(2)).unwrap();
+        game_handler.send(Command::Add(3)).unwrap();
+        game_handler.stop().unwrap();
+    }
+
+    #[test]
+    fn receive_state() {
+        let game_handler = Game::start();
+        game_handler.send(Command::Add(2)).unwrap();
+        let state = game_handler.last_state().unwrap();
+        assert_eq!(state, State { test_value: 2 });
+        game_handler.stop().unwrap();
     }
 }
