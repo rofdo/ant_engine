74bc84d50e1113b1d4acc48d71bbc07a2c26564b
rofdo <rofdo.github@ewe.net>
2024-06-12 16:43:11 +0200

    useful stuff for debuging

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod server;
 pub mod client;
+pub mod utils;
 mod shared;
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,5 @@
 #[cfg(test)]
 mod server_tests {
     #[test]
-    fn test_networker() {
-    }
+    fn test_networker() {}
 }
