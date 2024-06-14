12b466f93a4435fd101d9c5e4f0299588d1d8bb4
rofdo <rofdo.github@ewe.net>
2024-06-14 13:30:48 +0200

    vulkano first steps

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,23 @@
+use log::info;
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::VulkanLibrary;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default())
+        .expect("failed to create Vulkan instance");
+
+    let mut physical_devices = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate physical devices");
+    info!("Found {} compatible physical devices", physical_devices.len());
+
+    let physical_device = physical_devices
+        .next()
+        .expect("no physical device available");
+
+    for family in physical_device.queue_family_properties() {
+        info!("Found a queue family with: {:?} queue(s)", family);
+    }
 }
