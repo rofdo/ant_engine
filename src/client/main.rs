55656ded38b3192fb0d46a0af825671857fb99a8
rofdo <rofdo.github@ewe.net>
2024-06-15 13:59:41 +0200

    moving on

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,10 +1,16 @@
 use log::info;
-use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags, Queue};
-use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::VulkanLibrary;
+use std::default;
 use std::sync::Arc;
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::command_buffer::allocator::{
+    StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo,
+};
+use vulkano::device::{Device, DeviceCreateInfo, Queue, QueueCreateInfo, QueueFlags};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
+use vulkano::VulkanLibrary;
 
-fn initialize() -> (Arc<Instance>, Arc<Queue>) {
+fn initialize() -> (Arc<Instance>, Arc<Device>, Arc<Queue>) {
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
     let instance = Instance::new(library, InstanceCreateInfo::default())
         .expect("failed to create Vulkan instance");
@@ -29,7 +35,7 @@ fn initialize() -> (Arc<Instance>, Arc<Queue>) {
         })
         .expect("could not find a queue family supporting graphics")
         as u32;
-    let (_device, mut queues) = Device::new(
+    let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo {
             // the desired queue family by index
@@ -42,10 +48,50 @@ fn initialize() -> (Arc<Instance>, Arc<Queue>) {
     )
     .expect("failed to create device");
     let queue = queues.next().expect("no queue found");
-    (instance, queue)
+    (instance, device, queue)
+}
+
+fn move_stuff(memory_allocator: Arc<StandardMemoryAllocator>, device: Arc<Device>) {
+    let source_content: Vec<i32> = (0..64).collect();
+    let source = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_SRC,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        source_content,
+    )
+    .expect("failed to create source buffer");
+
+    let destination_content = vec![0; 64];
+    let destination = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        destination_content,
+    )
+    .expect("failed to create destination buffer");
+
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
 }
 
 fn main() {
     env_logger::init();
-    let (_instance, _queue) = initialize();
+    let (_instance, device, _queue) = initialize();
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 }
