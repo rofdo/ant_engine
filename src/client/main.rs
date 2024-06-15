8987f0147046eec79be59e61ab0458b105e415a8
rofdo <rofdo.github@ewe.net>
2024-06-15 16:58:38 +0200

    refactor

--- src/client/main.rs
+++ src/client/main.rs
@@ -192,7 +192,8 @@ fn window_size_dependent_setup(
         .collect::<Vec<_>>()
 }
 
-fn initialize() -> (Arc<Instance>, Arc<Device>, Arc<Queue>) {
+fn main() {
+    env_logger::init();
     let device_extensions = DeviceExtensions {
         khr_swapchain: true,
         ..DeviceExtensions::empty()
@@ -236,88 +237,7 @@ fn initialize() -> (Arc<Instance>, Arc<Device>, Arc<Queue>) {
         }
         Event::RedrawEventsCleared => {
             // render here
-        },
+        }
         _ => {}
     });
-
-    let mut physical_devices = instance
-        .enumerate_physical_devices()
-        .expect("could not enumerate physical devices");
-    info!(
-        "Found {} compatible physical devices",
-        physical_devices.len()
-    );
-    let physical_device = physical_devices
-        .next()
-        .expect("no physical device available");
-    let queue_family_index = physical_device
-        .queue_family_properties()
-        .iter()
-        .enumerate()
-        .position(|(_queue_family_index, queue_family_properties)| {
-            queue_family_properties
-                .queue_flags
-                .contains(QueueFlags::GRAPHICS)
-        })
-        .expect("could not find a queue family supporting graphics")
-        as u32;
-    let (device, mut queues) = Device::new(
-        physical_device,
-        DeviceCreateInfo {
-            // the desired queue family by index
-            queue_create_infos: vec![QueueCreateInfo {
-                queue_family_index,
-                ..Default::default()
-            }],
-            ..Default::default()
-        },
-    )
-    .expect("failed to create device");
-    let queue = queues.next().expect("no queue found");
-    (instance, device, queue)
-}
-
-fn move_stuff(memory_allocator: Arc<StandardMemoryAllocator>, device: Arc<Device>) {
-    let source_content: Vec<i32> = (0..64).collect();
-    let source = Buffer::from_iter(
-        memory_allocator.clone(),
-        BufferCreateInfo {
-            usage: BufferUsage::TRANSFER_SRC,
-            ..Default::default()
-        },
-        AllocationCreateInfo {
-            memory_type_filter: MemoryTypeFilter::PREFER_HOST
-                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
-            ..Default::default()
-        },
-        source_content,
-    )
-    .expect("failed to create source buffer");
-
-    let destination_content = vec![0; 64];
-    let destination = Buffer::from_iter(
-        memory_allocator.clone(),
-        BufferCreateInfo {
-            usage: BufferUsage::TRANSFER_DST,
-            ..Default::default()
-        },
-        AllocationCreateInfo {
-            memory_type_filter: MemoryTypeFilter::PREFER_HOST
-                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
-            ..Default::default()
-        },
-        destination_content,
-    )
-    .expect("failed to create destination buffer");
-
-    let command_buffer_allocator = StandardCommandBufferAllocator::new(
-        device.clone(),
-        StandardCommandBufferAllocatorCreateInfo::default(),
-    );
-}
-
-fn main() {
-    env_logger::init();
-    let (_instance, device, _queue) = initialize();
-    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 }
